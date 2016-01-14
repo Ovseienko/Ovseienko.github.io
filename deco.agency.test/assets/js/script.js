8348842e66e8c723d66baae64aa006ef1e2ab338
buruki <ovseienko@gmail.com>
2016-01-14 10:04:05 +0200

    -

--- deco.agency.test/assets/js/script.js
+++ deco.agency.test/assets/js/script.js
@@ -1,78 +1,33 @@
-	function showPrices(){
+function showPrices(){
 
 		var optimizationPrices = document.body.querySelector('.optimization-prices');
 		var borderBlock = document.body.getElementsByClassName('services-block')[1];
 		var optimizationPricesForMob = document.body.querySelector('.optimization-prices.for-mob');
 
 	if(window.innerWidth >= 769){
-
 		if (borderBlock.classList.contains('show-border')) {
-		optimizationPrices.style.cssText = "right:0%;opacity:0;z-index:-10; ";
+		optimizationPrices.classList.remove('show-optimization');
 		borderBlock.classList.remove('show-border');
 		}else{
-		optimizationPrices.style.cssText = "right:50%;opacity:1;z-index:10; ";
+		optimizationPrices.classList.add('show-optimization');
 		borderBlock.classList.add('show-border');
 		};
-
 	}else{
-		console.log(optimizationPricesForMob.style.display);
-		if (optimizationPricesForMob.style.display == 'none' || optimizationPricesForMob.style.display == "") {
-			optimizationPricesForMob.style.display = 'block';
+		if (optimizationPricesForMob.classList.contains('display')) {
+			optimizationPricesForMob.classList.remove('display');
 		}else{
-			optimizationPricesForMob.style.display = 'none';
+			optimizationPricesForMob.classList.add('display');
 		};
 	}
 		
-	}
-	function showPrices(){
+}
 
-		var optimizationPrices = document.body.querySelector('.optimization-prices');
-		var borderBlock = document.body.getElementsByClassName('services-block')[1];
-		var optimizationPricesForMob = document.body.querySelector('.optimization-prices.for-mob');
-
-	if(window.innerWidth >= 769){
-
-		if (borderBlock.classList.contains('show-border')) {
-		optimizationPrices.style.cssText = "right:0%;opacity:0;z-index:-10; ";
-		borderBlock.classList.remove('show-border');
-		}else{
-		optimizationPrices.style.cssText = "right:50%;opacity:1;z-index:10; ";
-		borderBlock.classList.add('show-border');
-		};
-
-	}else{
-		console.log(optimizationPricesForMob.style.display);
-		if (optimizationPricesForMob.style.display == 'none' || optimizationPricesForMob.style.display == "") {
-			optimizationPricesForMob.style.display = 'block';
-		}else{
-			optimizationPricesForMob.style.display = 'none';
-		};
-	}
-		
-	}
-
-	 showList = function(){
-
+showList = function(){
 		var select = document.body.getElementsByClassName('select')[0];
-		if(select.style.display == "none" || select.style.display == ""){
-			select.style.display = "block";
-
+		if(select.classList.contains('display-select')){
+			select.classList.remove('display-select');
 		}else{
-			select.style.display = "none";
+			select.classList.add('display-select');
 		}
+}
 
-	}
-
-
-
-	 showList = function(){
-
-		var select = document.body.getElementsByClassName('select')[0];
-		if(select.style.display == "none" || select.style.display == ""){
-			select.style.display = "block";
-
-		}else{
-			select.style.display = "none";
-		}
-
-	}
