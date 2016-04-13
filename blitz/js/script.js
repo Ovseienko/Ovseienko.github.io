3eb5d63f00b6307a0b3d390a2b07ae3597de0f22
buruki <ovseienko@gmail.com>
2016-04-13 19:24:53 +0300

    -

---
+++ blitz/js/script.js
@@ -0,0 +1,34 @@
+
+$(function() {
+	$('.jcarousel')
+		.on('jcarousel:create jcarousel:reload', function() {
+		var element = $(this),
+		width = element.innerWidth();
+		element.jcarousel('items').css('width', width + 'px');
+		})
+		.jcarousel( {
+		wrap: 'circular'
+		} );
+	$('.jcarousel-pagination')
+		.on('jcarouselpagination:active', 'a', function() {
+		$(this).addClass('active');
+		})
+		.on('jcarouselpagination:inactive', 'a', function() {
+		$(this).removeClass('active');
+		})
+	.jcarouselPagination({
+		item: function(page) {
+			return '<a href="#' + page + '">' + page + '</a>';
+		}
+	});
+	$('.jcarousel').jcarouselAutoscroll({
+		interval: 3000,
+		target: '+=1',
+		autostart: true
+	});
+	$('.show-nav').click(function(e){
+		e.preventDefault();
+		$('.mobile-nav').slideToggle('slow');
+		return false;
+	});
+});
