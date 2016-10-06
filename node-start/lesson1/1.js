a28333e147d676445b84442fd3d72f39e0aaa4c1
Valentina Ovseienko <ovseienko@gmail.com>
2016-10-06 10:04:31 +0300

    deleted folder

--- node-start/lesson1/1.js
+++
@@ -1,7 +0,0 @@
-nameInReverse = function(name){
-	var reversed = name.toLowerCase().split('').reverse().join(''),
-        result = reversed.charAt(0).toUpperCase() + reversed.slice(1);
-  
-	console.log('Your name in reverse is ' + result);
-};
-nameInReverse('Valentina');
