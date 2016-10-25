b50c8e78904ff5e11b6d1a723e19787d31683351
Valentina Ovseienko <ovseienko@gmail.com>
2016-10-25 15:00:52 +0300

    -

--- Single-page-with-AngularJS/week3/app.js
+++ Single-page-with-AngularJS/week3/app.js
@@ -5,53 +5,58 @@
         .directive('foundItems', FoundItems)
         .constant('urlPath', "https://davids-restaurant.herokuapp.com/menu_items.json");
 
-  NarrowItDownController.$inject = ['MenuSearchService'];
-  function NarrowItDownController(MenuSearchService) {
-    var ctrl = this;
-    var promise = MenuSearchService.getMenuCategories();
-    promise.then(function (response) {
-      ctrl.list = response.data;
-    });
-
-    ctrl.showItems = function () {
-      ctrl.menuItems = ctrl.list.menu_items;
-      ctrl.matchingItemsList = [];
-      for (var i = 0; i < ctrl.menuItems.length; i++) {
-        if(ctrl.menuItems[i].name.toLowerCase().indexOf(ctrl.searchItem.toLowerCase()) !== -1){
-          ctrl.matchingItemsList.push(ctrl.menuItems[i]);
-        }
-      }
-    };
-
-    ctrl.removeItem = function (index) {
-      ctrl.matchingItemsList.splice(index,1);
-    };
-
-  };
-
-  MenuSearchService.$inject = ['$http', 'urlPath']
-  function MenuSearchService($http, urlPath) {
-    var service = this;
-
-    service.getMenuCategories = function () {
-      var response = $http({
-        method: "GET",
-        url: urlPath
-      });
-      return response;
-    };
-
-  };
-
   function FoundItems() {
     var ddo = {
-      templateUrl: "./item.html"
-      // ,
-      // scope: {
-      //   onRemove: '&'
-      // }
+      templateUrl: './item.html',
+      scope: {
+        items: '<'
+      },
+      controller: NarrowItDownController,
+      controllerAs: 'ctrl',
+      bindToController: true
     };
+
     return ddo;
-  };
+  }
+
+  NarrowItDownController.$inject = ['MenuSearchService'];
+
+    function NarrowItDownController(MenuSearchService) {
+      var ctrl = this;
+
+      var promise = MenuSearchService.getMenuCategories();
+      promise.then(function (response) {
+        ctrl.list = response.data;
+        ctrl.menuItems = ctrl.list.menu_items;
+        // console.log(ctrl.menuItems);
+      });
+
+      ctrl.showItems = function () {
+        ctrl.matchingItemsList = [];
+        for (var i = 0; i < ctrl.menuItems.length; i++) {
+          if(ctrl.menuItems[i].name.toLowerCase().indexOf(ctrl.searchItem.toLowerCase()) !== -1){
+            ctrl.matchingItemsList.push(ctrl.menuItems[i]);
+          }
+        }
+      };
+
+      ctrl.removeItem = function (index) {
+        ctrl.matchingItemsList.splice(index, 1);
+      };
+
+    }
+
+    MenuSearchService.$inject = ['$http', 'urlPath'];
+    function MenuSearchService($http, urlPath) {
+      var service = this;
+
+      service.getMenuCategories = function () {
+        var response = $http({
+          method: "GET",
+          url: urlPath
+        });
+        return response;
+      };
+    }
 
 })();
