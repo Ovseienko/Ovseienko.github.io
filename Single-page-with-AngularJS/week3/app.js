54a67abc68151a1d06b20abe61518fe8a6ed1cc1
Valentina Ovseienko <ovseienko@gmail.com>
2016-10-26 15:19:03 +0300

    -

--- Single-page-with-AngularJS/week3/app.js
+++ Single-page-with-AngularJS/week3/app.js
@@ -2,22 +2,34 @@
   angular.module('NarrowItDownApp', [])
         .controller('NarrowItDownController', NarrowItDownController)
         .service('MenuSearchService', MenuSearchService)
-        .directive('foundItems', FoundItems)
+        .directive('foundItems', FoundItemsDirective)
         .constant('urlPath', "https://davids-restaurant.herokuapp.com/menu_items.json");
 
-  function FoundItems() {
+  function FoundItemsDirective() {
     var ddo = {
       templateUrl: './item.html',
       scope: {
         items: '<',
         onRemove: '&'
       },
-      controller: NarrowItDownController,
-      controllerAs: 'ctrl',
+      controller: NarrowItDownDirectiveController,
+      controllerAs: 'list',
       bindToController: true
     };
 
     return ddo;
+  }
+
+  function NarrowItDownDirectiveController() {
+    var list = this;
+
+    list.empty = function() {
+      return list.items && list.items.length === 0;
+    };
+
+    list.notEmpty = function() {
+      return list.items && list.items.length > 0;
+    };
   }
 
   NarrowItDownController.$inject = ['MenuSearchService'];
@@ -25,40 +37,59 @@
     function NarrowItDownController(MenuSearchService) {
       var ctrl = this;
 
-      var promise = MenuSearchService.getMenuCategories();
-      promise.then(function (response) {
-        ctrl.list = response.data;
-        ctrl.menuItems = ctrl.list.menu_items;
-        // console.log(ctrl.menuItems);
-      });
-      ctrl.matchingItemsList = [];
+      ctrl.searchItem = '';
 
-      ctrl.showItems = function () {
-        for (var i = 0; i < ctrl.menuItems.length; i++) {
-          if(ctrl.menuItems[i].name.toLowerCase().indexOf(ctrl.searchItem.toLowerCase()) !== -1){
-            ctrl.matchingItemsList.push(ctrl.menuItems[i]);
-          }
+      ctrl.searchForItem = function () {
+        if (ctrl.searchItem === '') {
+          ctrl.found = [];
+        }else{
+          MenuSearchService.getMatchedItems(ctrl.searchItem)
+          .then(function (result) {
+            ctrl.found = result;
+          }).catch(function (error) {
+            console.log(error);
+          });
         }
-        console.log(ctrl.matchingItemsList);
       };
-
-      ctrl.removeItem = function (itemIndex) {
-          ctrl.matchingItemsList.splice(itemIndex, 1);
+      ctrl.removeItem = function (index) {
+        ctrl.found.splice(index,1);
       };
+      // var promise = MenuSearchService.getMenuCategories();
+      // promise.then(function (response) {
+      //   ctrl.list = response.data;
+      //   ctrl.menuItems = ctrl.list.menu_items;
+      //   // console.log(ctrl.menuItems);
+      // });
+      // ctrl.matchingItemsList = [];
+      //
+      // ctrl.showItems = function () {
+      //   for (var i = 0; i < ctrl.menuItems.length; i++) {
+      //     if(ctrl.menuItems[i].name.toLowerCase().indexOf(ctrl.searchItem.toLowerCase()) !== -1){
+      //       ctrl.matchingItemsList.push(ctrl.menuItems[i]);
+      //     }
+      //   }
+      //   console.log(ctrl.matchingItemsList);
+      // };
+      //
+      // ctrl.removeItem = function (itemIndex) {
+      //     ctrl.matchingItemsList.splice(itemIndex, 1);
+      // };
 
     }
 
-var items = [];
 
-    MenuSearchService.$inject = ['$http', 'urlPath'];
-    function MenuSearchService($http, urlPath) {
+    MenuSearchService.$inject = ['$http', 'urlPath', '$filter'];
+    function MenuSearchService($http, urlPath, $filter) {
       var service = this;
-      service.getMenuCategories = function () {
-        var response = $http({
+
+      service.getMatchedItems = function (searchItem) {
+        return $http({
           method: "GET",
           url: urlPath
+        }).then(function (result) {
+          var foundItems = result.data.menu_items;
+          return $filter('filter')(foundItems, {description: searchItem});
         });
-        return response;
       };
 
     }
