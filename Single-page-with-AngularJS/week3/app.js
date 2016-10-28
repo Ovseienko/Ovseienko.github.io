b3cc295c0cbe401a4aff52d8fd6617495c0a0cfb
Valentina Ovseienko <ovseienko@gmail.com>
2016-10-28 15:05:56 +0300

    -

--- Single-page-with-AngularJS/week3/app.js
+++ Single-page-with-AngularJS/week3/app.js
@@ -1,97 +1,79 @@
 (function () {
   angular.module('NarrowItDownApp', [])
-        .controller('NarrowItDownController', NarrowItDownController)
-        .service('MenuSearchService', MenuSearchService)
-        .directive('foundItems', FoundItemsDirective)
-        .constant('urlPath', "https://davids-restaurant.herokuapp.com/menu_items.json");
+    .controller('NarrowItDownController', NarrowItDownController)
+    .service('MenuSearchService', MenuSearchService)
+    .directive('foundItems', FoundItemsDirective)
+    .constant('urlPath', "https://davids-restaurant.herokuapp.com/menu_items.json");
 
-  function FoundItemsDirective() {
-    var ddo = {
-      templateUrl: './item.html',
-      scope: {
-        items: '<',
-        onRemove: '&'
-      },
-      controller: NarrowItDownDirectiveController,
-      controllerAs: 'list',
-      bindToController: true
-    };
-
-    return ddo;
-  }
-
-  function NarrowItDownDirectiveController() {
-    var list = this;
-
-    list.empty = function() {
-      return list.items && list.items.length === 0;
-    };
-
-    list.notEmpty = function() {
-      return list.items && list.items.length > 0;
-    };
-  }
-
-  NarrowItDownController.$inject = ['MenuSearchService'];
-
+    NarrowItDownController.$inject = ['MenuSearchService'];
     function NarrowItDownController(MenuSearchService) {
       var ctrl = this;
 
       ctrl.searchItem = '';
+      ctrl.errorMsg = '';
+      ctrl.loading = false;
 
       ctrl.searchForItem = function () {
+        ctrl.errorMsg = '';
         if (ctrl.searchItem === '') {
+          ctrl.errorMsg = "Nothing found!";
           ctrl.found = [];
-        }else{
+        }else {
+          ctrl.loading = true;
           MenuSearchService.getMatchedItems(ctrl.searchItem)
           .then(function (result) {
+            ctrl.errorMsg = '';
             ctrl.found = result;
+            ctrl.loading = false;
           }).catch(function (error) {
             console.log(error);
           });
         }
       };
+
       ctrl.removeItem = function (index) {
         ctrl.found.splice(index,1);
       };
-      // var promise = MenuSearchService.getMenuCategories();
-      // promise.then(function (response) {
-      //   ctrl.list = response.data;
-      //   ctrl.menuItems = ctrl.list.menu_items;
-      //   // console.log(ctrl.menuItems);
-      // });
-      // ctrl.matchingItemsList = [];
-      //
-      // ctrl.showItems = function () {
-      //   for (var i = 0; i < ctrl.menuItems.length; i++) {
-      //     if(ctrl.menuItems[i].name.toLowerCase().indexOf(ctrl.searchItem.toLowerCase()) !== -1){
-      //       ctrl.matchingItemsList.push(ctrl.menuItems[i]);
-      //     }
-      //   }
-      //   console.log(ctrl.matchingItemsList);
-      // };
-      //
-      // ctrl.removeItem = function (itemIndex) {
-      //     ctrl.matchingItemsList.splice(itemIndex, 1);
-      // };
 
     }
 
 
-    MenuSearchService.$inject = ['$http', 'urlPath', '$filter'];
-    function MenuSearchService($http, urlPath, $filter) {
+    MenuSearchService.$inject = ['$http', 'urlPath'];
+    function MenuSearchService($http, urlPath) {
       var service = this;
 
-      service.getMatchedItems = function (searchItem) {
+      service.getMatchedItems = function (searchI) {
         return $http({
           method: "GET",
           url: urlPath
         }).then(function (result) {
           var foundItems = result.data.menu_items;
-          return $filter('filter')(foundItems, {description: searchItem});
+          foundItems.filter(function (el) {
+            return el.description.toLowerCase().indexOf(searchI.toLowerCase) !== -1;
+          });
+          return foundItems;
         });
       };
 
+    }
+
+    function FoundItemsDirective() {
+      var ddo = {
+        templateUrl: './item1.html',
+        scope:{
+          items: '<',
+          onRemove: '&'
+        },
+        controller: NarrowItDownDirectiveController,
+        controllerAs: 'list',
+        bindToController: true
+      };
+
+      return ddo;
+    }
+
+    function NarrowItDownDirectiveController() {
+      var list = this;
     }
 
 })();
